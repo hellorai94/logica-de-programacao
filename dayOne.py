e33fe4105d805b665085ad9d77c4bc170a0aa10c
raissa <uefsraissa@gmail.com>
2022-03-22 16:17:24 -0300

    duas novas questões

---
+++ dayOne.py
@@ -0,0 +1,34 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2 + 2
+4
+2
+2
+5 - 3
+2
+3 * 7
+21
+22 / 7
+3.142857142857143
+2 + 3 * 6
+20
+(2 + 3) * 6
+30
+(5 - 1)*((7 + 1)/(3 - 1))
+16.0
+16.0
+16.0
+5 +
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+-2
+-2
+"Hello world"
+'Hello world'
+"Alice" + "Bob"
+'AliceBob'
+"Alice" * 3
+'AliceAliceAlice'
+"Hello" + "!" * 10
+'Hello!!!!!!!!!!'
---
+++ questaoSeis.py
@@ -0,0 +1,13 @@
+# This program says hello and aks for my name
+
+print("Hello World!")
+
+print("What is your name?") #aks for their name
+myName = input()
+print("It is good to meet you, " + myName)
+print("The length of your name is:")
+print(len(myName))
+
+print("What is you age?") #ask for their age
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
