7f4d68ca7295fabc772dd2ca0d65d0a0fb66ef31
raissa <uefsraissa@gmail.com>
2022-04-06 22:03:41 -0300

    tentando passar as atividades feitas em portugol para javaScript

---
+++ javaScript/questaoUm.js
@@ -0,0 +1,5 @@
+let num1 = 9;
+let num2 = 5;
+
+media = (num1 + num2) / 2;
+console.log("A média do aluno é ", media);
--- python/questaoVinteQuatro.py
+++ python/questaoVinteQuatro.py
@@ -1,9 +1,13 @@
 print("How many cats do you have?")
 numCats = input()
 try:
-    if int(numCats)>= 4:
+    if int(numCats)< 0:
+        print("Only positive numbers")
+    elif int(numCats)>= 4:
         print("That is a lot of cats.")
     else:
         print("That is not that many cats.")
 except RuntimeError:
+    print("Only int numbers")
+except ValueError:
     print("Only int numbers")
